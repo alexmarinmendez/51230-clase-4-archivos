23c975bbbf1acf89a1d521dcda456bc4f54a01a1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-03 09:26:11 -0500

    feat: fs promises

---
+++ 03-archivos_promises.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+const filename = './archivo_promesas.txt'
+
+const operacionesAsync = async() => {
+    await fs.promises.writeFile(filename, 'Saludos desde Coder!')
+
+    // try {
+    //     let contenido = await fs.promises.readFile('./otro_file.txt', 'utf-8')
+    //     console.log(contenido)
+    // } catch (err) {
+    //     console.log(err)
+    // }
+
+    let contenido = await fs.promises.readFile(filename, 'utf-8')
+    console.log(contenido)
+
+    await fs.promises.appendFile(filename, '\nOtros saludos.')
+
+    await fs.promises.unlink(filename)
+}
+
+operacionesAsync()
